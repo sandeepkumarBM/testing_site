d1a499c54d463a247a7c431995f5335cad40499d
sandeepkumarBM <84248574+sandeepkumarBM@users.noreply.github.com>
2024-03-05 21:53:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$('.testimonial-container').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    margin:10,
+    nav:true,
+    navText:["<i class='fa-solid fa-arrow-left'></i>",
+    "<i class='fa-solid fa-arrow-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        }
+    }
+})
